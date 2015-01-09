c93349d778dfd2d513e6f34e6d016d0d8e3005c8
zimbra-pmason <pmason@zimbra.com>
2015-01-09 11:00:28 -0600

    Initial Commit

---
+++ src/Api.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Configuration;
+using Telligent.Evolution.Extensibility.OAuthClient.Version1;
+using Telligent.Evolution.Extensibility.UI.Version1;
+
+
+namespace Zimbra.Social.RemotingSDK.Sitecore
+{
+    public static class Api
+    {
+        public static RemoteScriptedContentFragmentHost GetHost(string hostName,bool forceRegister = false)
+        {
+            var config = Factory.GetConfigNode("Zimbra/hosts/host[@name='" + hostName + "']");
+            if (config == null)
+                throw new ConfigurationErrorsException("host configuration has not been defined for this object");
+
+            var id = config.GetGuidAttributeValueOrDefault("id", null);
+            if (id == null || !id.HasValue)
+                throw new ConfigurationErrorsException("host node must contain a valid guid id");
+
+            
+            var host = RemoteScriptedContentFragmentHost.Get(id.Value);
+            if (host == null && forceRegister)
+            {
+                host = new SitecoreHost(hostName);
+                Register(host as SitecoreHost);
+            }
+               
+
+            return host;
+        }
+
+        public static void Register(SitecoreHost host)
+        {
+            RemoteScriptedContentFragmentHost.Register(host);
+            OAuthAuthentication.RegisterConfiguration(host);
+        }
+    }
+}
---
+++ src/Web/rsw.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" Class="Telligent.Evolution.Extensibility.UI.Version1.RemoteScriptedContentFragmentsHttpHandler, Telligent.Evolution.RemoteScriptedContentFragments"  %>
