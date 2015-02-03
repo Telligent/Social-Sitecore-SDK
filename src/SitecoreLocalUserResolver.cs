bf033153a30de466515cb403039179b3f2232503
zimbra-pmason <pmason@zimbra.com>
2015-02-03 13:48:04 -0600

    Updated for new Framework

---
+++ src/SitecoreLocalUserResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore;
+using Telligent.Evolution.Extensibility.Rest.Version1;
+
+namespace Telligent.SitecoreSDK
+{
+    public class SitecoreLocalUserResolver:ILocalUserResolver
+    {
+        public LocalUser GetLocalUserDetails(System.Web.HttpContextBase context, Host host)
+        {
+
+            if (Context.User == null || !Context.User.IsAuthenticated)
+                return null;
+
+            return new LocalUser(Context.User.LocalName, Context.User.Profile.Email);
+        }
+    }
+
+    public class SitecoreLocalUserResolverWithDomain : ILocalUserResolver
+    {
+        public LocalUser GetLocalUserDetails(System.Web.HttpContextBase context, Host host)
+        {
+
+            if (Context.User == null || !Context.User.IsAuthenticated)
+                return null;
+
+            return new LocalUser(Context.User.Name, Context.User.Profile.Email);
+        }
+    }
+}
--- src/Web/oauth.ashx
+++ src/Web/oauth.ashx
@@ -1,1 +1,1 @@
-﻿<%@ WebHandler Language="C#" Class="Telligent.Evolution.Extensibility.OAuthClient.Version1.OAuthClientHttpHandler, Telligent.Evolution.Extensions.OAuthAuthentication"  %>
+﻿<%@ WebHandler Language="C#" Class="Telligent.Evolution.Extensibility.OAuthClient.Version1.OAuthClientHttpHandler, Telligent.Rest.SDK"  %>
