85ed4c10fcb508c995791b6bae4c1a7f14e077be
zimbra-pmason <pmason@zimbra.com>
2015-05-04 10:34:26 -0500

    Adjusted for 1.0.40

---
+++ src/Pipelines/IntializeHost.cs
@@ -0,0 +1,34 @@
+﻿
+using Sitecore;
+using Sitecore.Pipelines;
+using Telligent.Evolution.Extensibility.Rest.Version1;
+
+namespace Telligent.Rest.SDK.Sitecore.Pipelines
+{
+    public class InitializeHost
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+           
+
+
+            Host.Get("default").ResolveLocalUser = (host, resolveArgs) =>
+            {
+                if (Context.User == null || !Context.User.IsAuthenticated)
+                {
+                    return null; 
+                }
+                return new LocalUser(Context.User.Name, Context.User.Profile.Email);
+            };
+
+            //If you do not wish to use domain accounts
+            //Host.Get("default").ResolveLocalUser = (host, resolveArgs) =>
+            //{
+            //    if (Context.User == null || !Context.User.IsAuthenticated)
+            //        return null;
+
+            //    return new LocalUser(Context.User.LocalName, Context.User.Profile.Email);
+            //};
+        }
+    }
+}
--- src/oauth.ashx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebHandler Language="C#" Class="Telligent.Evolution.Extensibility.OAuthClient.Version1.OAuthClientHttpHandler, Telligent.Rest.SDK"  %>
